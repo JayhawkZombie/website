629b7e1963297189f4ddadbd88dbbc4f6221f8b4
Kurt Slagle <kurt_slagle@yahoo.com>
2026-02-28 12:51:10 -0800

    Always have dark mode showing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import { MantineProviders } from "@/lib/mantine/provider";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import "@/styles/globals.scss";
 import styles from "./layout.module.scss";
-import { MantineProvider, createTheme } from "@mantine/core";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import { theme } from "@/lib/mantine/theme";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 
@@ -17,13 +17,12 @@ export const metadata: Metadata = {
 	description: "Personal website portfolio, blog, and resume",
 };
 
-const theme = createTheme({
-	/** Put your mantine theme override here */
-});
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
+			<head>
+				<ColorSchemeScript defaultColorScheme="dark" />
+			</head>
 			<body className={inter.className + " " + styles.body}>
 				<MantineProvider theme={theme} defaultColorScheme="dark">
 					<Notifications />
